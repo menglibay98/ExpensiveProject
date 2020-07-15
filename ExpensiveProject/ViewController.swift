fe6bf08fc05370a82e3d49c87e67933071b4291e
Islam Menglibay <iskomenlibay@gmail.com>
2020-07-15 18:35:26 +0600

    Initial Commit

---
+++ ExpensiveProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExpensiveProject
+//
+//  Created by Ислам on 7/15/20.
+//  Copyright © 2020 KaspiLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
